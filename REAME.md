8bf4a29474219cb9ebc7e14437506f2c08762c60
Olaf Rogalsky <olaf.rogalsky@gmail.com>
2023-12-28 15:38:34 +0100

    add code and readme for the module

---
+++ REAME.md
@@ -0,0 +1,26 @@
+# TerminalSize.jl
+TerminalSize is a very small library to read out the current terminal size (characters and pixels).
+
+The library exports only two symbols:
+## get_terminal_size
+Retrieves the size of the terminal from the tty device.
+```julia
+get_terminal_size()::TermSize
+get_terminal_size(tty::Base.TTY)::TermSize
+get_terminal_size(raw_fd::Base.RawFD)::TermSize
+get_terminal_size(fd::Integer)::TermSize
+```
+## TermSize
+Structure, whhich holds the result.
+```julia
+mutable struct TermSize
+    row::UInt16                 # number of rows
+    col::UInt16                 # number of columns
+    xpixel::UInt16              # number of pixel in x-direction
+    ypixel::UInt16              # number of pixel in x-direction
+    cells::NTuple{2, UInt16}    # tuple with (col, row)
+    pixels::NTuple{2, UInt16}   # tuple with (xpixel, ypixel)
+end
+```
+
+
--- src/TerminalSize.jl
+++ src/TerminalSize.jl
@@ -1,5 +1,35 @@
 module TerminalSize
 
-greet() = print("Hello World!")
+export TerminalSize, get_terminal_size
+
+mutable struct TermSize
+    row::UInt16
+    col::UInt16
+    xpixel::UInt16
+    ypixel::UInt16
+    cells::NTuple{2, UInt16}
+    pixels::NTuple{2, UInt16}
+    TermSize() = new(0, 0, 0, 0, (0, 0), (0, 0))
+end
+
+terminal_size::TermSize = TermSize()
+TIOCGWINSZ::Cint = 0x5413
+
+get_terminal_size()::TermSize = get_terminal_size(Base.stdout)
+get_terminal_size(tty::Base.TTY)::TermSize = get_terminal_size(Base._fd(tty))
+get_terminal_size(raw_fd::Base.RawFD)::TermSize = get_terminal_size(Base.bitcast(Cint, raw_fd))
+
+function get_terminal_size(fd::Integer)::TermSize
+    global terminal_size
+    ret = @ccall ioctl(fd::Cint, TIOCGWINSZ::Cint, terminal_size::Ref{TermSize})::Cint
+    if ret == -1
+        throw(Base.SystemError("in 'get_terminal_size': ioctl(fd = $(fd), request = TIOCGWINSZ, dataptr = $(repr(UInt(pointer_from_objref(terminal_size)))))"))
+    end
+    terminal_size.cells = (terminal_size.col, terminal_size.row)
+    terminal_size.pixels = (terminal_size.xpixel, terminal_size.ypixel)
+    return terminal_size
+end
+
+Base.show(io::IO, x::TermSize) = print(io, "TermSize(row = $(x.row), col = $(x.col), xpixel = $(x.xpixel), ypixel = $(x.ypixel))")
 
 end # module TerminalSize
